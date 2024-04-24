4dc75a183ef02d08eeb734fb13cce39f3916a089
Matthew Blott <m4ttb@pm.me>
2024-04-24 23:08:03 +0100

    Update .config/nvim/lua/user/config.lua

--- dot_config/nvim/lua/user/config.lua
+++ dot_config/nvim/lua/user/config.lua
@@ -228,37 +228,7 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({select = false}),
 
     -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-    -- Scroll up and down in the completion documentation
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-    ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            -- elseif luasnip.expand_or_locally_jumpable() then
-            --   luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --         if cmp.visible() then
-    --           cmp.select_prev_item()
-    --         elseif luasnip.jumpable(-1) then
-    --           luasnip.jump(-1)
-    --         else
-    --           fallback()
-    --         end
-    --       end, { "i", "s" }),
+    -- ['<C-Space>'] = cmp.mapping.complete(),
 
   })
 })
