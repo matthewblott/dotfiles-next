c7305afbd8346a8c5adf38c655d2a20deb17da95
Matthew Blott <m4ttb@pm.me>
2024-04-26 23:37:46 +0100

    Update .config/nvim/lua/user/config.lua

--- dot_config/nvim/lua/user/config.lua
+++ dot_config/nvim/lua/user/config.lua
@@ -77,10 +77,7 @@ keymap('v', '<S-Tab>', '<gv', opts)
 local telescope = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-
---vim.keymap.set('n', '<leader>fF', telescope.find_files, { hidden = true, no_ignore = true })
---vim.keymap.set('n', '<leader>fF', telescope.find_files, { hidden = true })
-
+vim.keymap.set('n', '<leader>fF', 'lua: require("telescope.builtin").find_files({ hidden = true, no_ignore = true })', {})
 vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
