27bf21e0f54cdb5165188b31ac3b771879f18761
Matthew Blott <m4ttb@pm.me>
2024-05-05 11:09:14 +0100

    saved

--- dot_config/nvim/lua/user/config.lua
+++ dot_config/nvim/lua/user/config.lua
@@ -204,9 +204,10 @@ vim.keymap.set('n', '<leader><leader>l', splits.swap_buf_right)
 -- the token won't be parsed properly otherwise and you'll receive an error.
 
 vim.keymap.set('i', '<c-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+-- vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+-- vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+-- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
 vim.keymap.set('i', '<c-?>', function() return vim.fn['codeium#Complete']() end, { expr = true, silent = true })
 
 
